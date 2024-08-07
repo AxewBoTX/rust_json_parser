76c460a9d83ee2ed6063b4311ccc21373a728d08
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-08-07 16:17:38 +0530

    feat(rust_toml_parser): complete refiner implementation
    the refiner implementation is complete, it refines all the
    `NonQuoteString` tokens into their respective specific tokens like
    `Boolean`, `Integer`, `Float` etc etc
    
    Other Changes:
    - docs: add links to all the parsers in the root `README.md`

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # parser-implementations
 
 various custom parser implementations in various languages
+
+following parser have been implemented (ranked by how proud I am of each one)
+
+- [rust_toml_parser](https://github.com/AxewBoTX/parser-implementations/tree/main/rust_toml_parser)
+- [rust_json_parser](https://github.com/AxewBoTX/parser-implementations/tree/main/rust_json_parser)
--- rust_toml_parser/src/refiner.rs
+++ rust_toml_parser/src/refiner.rs
@@ -10,90 +10,201 @@ impl Refiner {
             list: utils::IteratorList::new(list),
         };
     }
+    // main refining function
     pub fn refine_tokens(&mut self) -> Result<Vec<tokenizer::Token>, String> {
         let mut tokens: Vec<tokenizer::Token> = Vec::new();
         while let Some(current_token) = self.list.peek() {
-            // ----- every value that needs to be refined is a value in a key-value pair, including
-            // the things in a array, because the array itself will be a value in a key-value pair.
-            // So, instead of checking for key-value pair inside the match statement, check for it
-            // outside, it would make things easily -----
-            if current_token.kind == tokenizer::TokenKind::NonQuoteString {}
-            match current_token.kind {
-                // non-quote string
-                tokenizer::TokenKind::NonQuoteString => {
-                    if self
-                        .list
-                        .peek_before(1)
-                        .is_some_and(|prev_token| prev_token.kind == tokenizer::TokenKind::EqualTo)
-                    {
-                        // value in a key-value pair
-                        if [String::from("true"), String::from("false")]
-                            .contains(&current_token.value)
-                        {
-                            // boolean
-                            tokens.push(tokenizer::Token::new(
-                                tokenizer::TokenKind::Boolean,
-                                current_token.value.clone(),
-                            ));
-                            let _ = self.list.current();
-                        } else if utils::is_integer(&current_token.value) {
-                            // number
-                            let mut value = String::new();
-                            while let Some(next_token) = self.list.peek() {
-                                if next_token.kind == tokenizer::TokenKind::Dot
-                                    || next_token.kind == tokenizer::TokenKind::NonQuoteString
-                                        && utils::is_integer(&next_token.value)
-                                {
-                                    value.push_str(&next_token.value);
-                                    let _ = self.list.current();
-                                } else {
-                                    break;
-                                }
-                            }
-                            if utils::is_integer(&value) {
-                                // integer
-                                match value.parse::<i64>() {
-                                    Ok(safe_value) => {
-                                        tokens.push(tokenizer::Token::new(
-                                            tokenizer::TokenKind::Integer,
-                                            safe_value.to_string(),
-                                        ));
-                                    }
-                                    Err(e) => {
-                                        return Err(e.to_string());
-                                    }
-                                }
-                            } else if utils::is_float(&value) {
-                                // float
-                                match value.parse::<f64>() {
-                                    Ok(safe_value) => {
-                                        tokens.push(tokenizer::Token::new(
-                                            tokenizer::TokenKind::Float,
-                                            safe_value.to_string(),
-                                        ));
-                                    }
-                                    Err(e) => {
-                                        return Err(e.to_string());
-                                    }
-                                }
-                            } else {
-                                return Err(format!("Unexpected token: {:#?}", &value));
-                            }
-                        } else {
-                            tokens.push(current_token.clone());
-                            let _ = self.list.current();
+            if [
+                tokenizer::TokenKind::NonQuoteString,
+                tokenizer::TokenKind::BracketOpen,
+            ]
+            .contains(&current_token.kind)
+                && self
+                    .list
+                    .peek_before(1)
+                    .is_some_and(|prev_token| prev_token.kind == tokenizer::TokenKind::EqualTo)
+            {
+                match current_token.kind {
+                    // non-quote string
+                    tokenizer::TokenKind::NonQuoteString => match self.refine_non_quote_string() {
+                        Ok(safe_value) => {
+                            tokens.push(safe_value);
                         }
-                    } else {
+                        Err(e) => {
+                            return Err(e.to_string());
+                        }
+                    },
+                    // array
+                    tokenizer::TokenKind::BracketOpen => match self.refine_array() {
+                        Ok(safe_value) => {
+                            tokens.extend(safe_value);
+                        }
+                        Err(e) => {
+                            return Err(e.to_string());
+                        }
+                    },
+                    _ => {
                         tokens.push(current_token.clone());
-                        let _ = self.list.current();
+                        match self.list.current() {
+                            Some(_) => {}
+                            None => {
+                                return Err("something went terribly wrong! (most probably a language problem, not a code problem)".to_string());
+                            }
+                        }
                     }
                 }
-                _ => {
-                    tokens.push(current_token.clone());
-                    let _ = self.list.current();
+            } else {
+                tokens.push(current_token.clone());
+                match self.list.current() {
+                    Some(_) => {}
+                    None => {
+                        return Err("something went terribly wrong! (most probably a language problem, not a code problem)".to_string());
+                    }
                 }
             }
         }
         return Ok(tokens);
+    }
+    // refine `NonQuoteString` tokens
+    fn refine_non_quote_string(&mut self) -> Result<tokenizer::Token, String> {
+        match self.list.peek() {
+            Some(current_token) => {
+                // value in a key-value pair
+                if [String::from("true"), String::from("false")].contains(&current_token.value) {
+                    // boolean
+                    match self.list.current() {
+                        Some(_) => {}
+                        None => {
+                            return Err("something went terribly wrong! (most probably a language problem, not a code problem)".to_string());
+                        }
+                    }
+                    return Ok(tokenizer::Token::new(
+                        tokenizer::TokenKind::Boolean,
+                        current_token.value.clone(),
+                    ));
+                } else if utils::is_integer(&current_token.value) {
+                    match self.refine_number() {
+                        Ok(safe_value) => {
+                            return Ok(safe_value);
+                        }
+                        Err(e) => {
+                            return Err(e.to_string());
+                        }
+                    }
+                } else {
+                    match self.list.current() {
+                        Some(_) => {}
+                        None => {
+                            return Err("something went terribly wrong! (most probably a language problem, not a code problem)".to_string());
+                        }
+                    }
+                    return Ok(current_token.clone());
+                }
+            }
+            None => {
+                return Err("failed to refine non-quote string".to_string());
+            }
+        }
+    }
+    // refine array
+    fn refine_array(&mut self) -> Result<Vec<tokenizer::Token>, String> {
+        let mut tokens: Vec<tokenizer::Token> = Vec::new();
+        match self.list.current() {
+            Some(open_bracket) => {
+                tokens.push(open_bracket);
+            }
+            None => return Err("failed to refine arrray".to_string()),
+        }
+        while let Some(current_token) = self.list.peek() {
+            match current_token.kind {
+                // non-quote string
+                tokenizer::TokenKind::NonQuoteString => match self.refine_non_quote_string() {
+                    Ok(safe_value) => {
+                        tokens.push(safe_value);
+                    }
+                    Err(e) => {
+                        return Err(e.to_string());
+                    }
+                },
+                // array
+                tokenizer::TokenKind::BracketOpen => match self.refine_array() {
+                    Ok(safe_value) => {
+                        tokens.extend(safe_value);
+                    }
+                    Err(e) => {
+                        return Err(e.to_string());
+                    }
+                },
+                tokenizer::TokenKind::BracketClose => {
+                    tokens.push(current_token.clone());
+                    match self.list.current() {
+                        Some(_) => {}
+                        None => {
+                            return Err("something went terribly wrong! (most probably a language problem, not a code problem)".to_string());
+                        }
+                    }
+                    break;
+                }
+                _ => {
+                    tokens.push(current_token.clone());
+                    match self.list.current() {
+                        Some(_) => {}
+                        None => {
+                            return Err("something went terribly wrong! (most probably a language problem, not a code problem)".to_string());
+                        }
+                    }
+                }
+            }
+        }
+        return Ok(tokens);
+    }
+    // refine number
+    fn refine_number(&mut self) -> Result<tokenizer::Token, String> {
+        let mut value = String::new();
+        while let Some(next_token) = self.list.peek() {
+            if next_token.kind == tokenizer::TokenKind::Dot
+                || next_token.kind == tokenizer::TokenKind::NonQuoteString
+                    && utils::is_integer(&next_token.value)
+            {
+                value.push_str(&next_token.value);
+                match self.list.current() {
+                    Some(_) => {}
+                    None => {
+                        return Err("something went terribly wrong! (most probably a language problem, not a code problem)".to_string());
+                    }
+                }
+            } else {
+                break;
+            }
+        }
+        if utils::is_integer(&value) {
+            // integer
+            match value.parse::<i64>() {
+                Ok(safe_value) => {
+                    return Ok(tokenizer::Token::new(
+                        tokenizer::TokenKind::Integer,
+                        safe_value.to_string(),
+                    ));
+                }
+                Err(e) => {
+                    return Err(e.to_string());
+                }
+            }
+        } else if utils::is_float(&value) {
+            // float
+            match value.parse::<f64>() {
+                Ok(safe_value) => {
+                    return Ok(tokenizer::Token::new(
+                        tokenizer::TokenKind::Float,
+                        safe_value.to_string(),
+                    ));
+                }
+                Err(e) => {
+                    return Err(e.to_string());
+                }
+            }
+        } else {
+            return Err(format!("Unexpected token: {:#?}", &value));
+        }
     }
 }
