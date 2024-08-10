3add92c0e1226f79855efe07483ac6236ae6ae6a
AxewBoTX <lovedeepbrar54155@gmail.com>
2024-08-10 16:47:14 +0530

    feat(rust_toml_parser/ast_builder): add functionality to parse arrays and table headings
    NOTE: I have added functionality for simple and nested table headings
    but the program is still unable to parse table headings for array of
    tables like `[[]]`

--- rust_toml_parser/src/ast_builder.rs
+++ rust_toml_parser/src/ast_builder.rs
@@ -34,7 +34,7 @@ impl ASTBuilder {
                     if is_key == true {
                         current_key = Some(token.value.clone());
                     } else {
-                        return Err("that wasnt supposed to happen".to_string());
+                        return Err("something went terribly wrong! (most probably a language problem, not a code problem)".to_string());
                     }
                 }
                 tokenizer::TokenKind::EqualTo => {
@@ -54,7 +54,7 @@ impl ASTBuilder {
                         } else if token.value.to_lowercase() == "false".to_string() {
                             main_table.insert(key.to_string(), ASTNode::Boolean(false));
                         } else {
-                            return Err("that wasnt supposed to happen".to_string());
+                            return Err("something went terribly wrong! (most probably a language problem, not a code problem)".to_string());
                         }
                     }
                 }
@@ -91,13 +91,56 @@ impl ASTBuilder {
                 }
                 tokenizer::TokenKind::CurlyBracketOpen => {}
                 tokenizer::TokenKind::CurlyBracketClose => {}
-                tokenizer::TokenKind::BracketOpen => {}
+                tokenizer::TokenKind::BracketOpen => {
+                    if is_key == true {
+                        match self.parse_table_heading() {
+                            Ok((safe_key, safe_value)) => {
+                                main_table.insert(safe_key.to_string(), ASTNode::Table(safe_value));
+                            }
+                            Err(e) => {
+                                return Err(e.to_string());
+                            }
+                        }
+                    } else if let Some(key) = &current_key {
+                        match self.parse_array() {
+                            Ok(safe_value) => {
+                                main_table.insert(key.to_string(), ASTNode::Array(safe_value));
+                            }
+                            Err(e) => {
+                                return Err(e.to_string());
+                            }
+                        }
+                    }
+                }
                 tokenizer::TokenKind::BracketClose => {}
                 tokenizer::TokenKind::Dot => {}
                 tokenizer::TokenKind::Comma => {}
             }
         }
         return Ok(main_table);
+    }
+    // parse table heading
+    pub fn parse_table_heading(&mut self) -> Result<(String, HashMap<String, ASTNode>), String> {
+        let mut table: HashMap<String, ASTNode> = HashMap::new();
+        let mut table_heading: String = String::new();
+        while let Some(token) = self.list.current() {
+            match token.kind {
+                tokenizer::TokenKind::NonQuoteString | tokenizer::TokenKind::QuoteString => {
+                    table_heading = token.value.to_string();
+                }
+                tokenizer::TokenKind::Dot => match self.parse_table_heading() {
+                    Ok((safe_heading, safe_value)) => {
+                        table.insert(safe_heading, ASTNode::Table(safe_value));
+                    }
+                    Err(e) => {
+                        return Err(e.to_string());
+                    }
+                },
+                tokenizer::TokenKind::BracketClose => {}
+                _ => {}
+            }
+        }
+        return Ok((table_heading, table));
     }
     // parse `Table`
     pub fn parse_table(&mut self) -> HashMap<String, ASTNode> {
@@ -123,26 +166,63 @@ impl ASTBuilder {
         return table;
     }
 
-    pub fn parse_array(&mut self) -> Vec<ASTNode> {
-        let array: Vec<ASTNode> = Vec::new();
+    fn parse_array(&mut self) -> Result<Vec<ASTNode>, String> {
+        let mut array: Vec<ASTNode> = Vec::new();
 
-        while let Some(curr_token) = self.list.current() {
-            match curr_token.kind {
+        while let Some(token) = self.list.current() {
+            match token.kind {
                 tokenizer::TokenKind::CurlyBracketOpen => {}
                 tokenizer::TokenKind::CurlyBracketClose => {}
-                tokenizer::TokenKind::BracketOpen => {}
-                tokenizer::TokenKind::BracketClose => {}
-                tokenizer::TokenKind::EqualTo => {}
-                tokenizer::TokenKind::Dot => {}
+                tokenizer::TokenKind::BracketOpen => match self.parse_array() {
+                    Ok(safe_value) => array.push(ASTNode::Array(safe_value)),
+                    Err(e) => {
+                        return Err(e.to_string());
+                    }
+                },
+                tokenizer::TokenKind::BracketClose => {
+                    break;
+                }
+                tokenizer::TokenKind::EqualTo => {
+                    return Err(format!("unexpected token: {:#?}", token).to_string());
+                }
+                tokenizer::TokenKind::Dot => {
+                    return Err(format!("unexpected token: {:#?}", token).to_string());
+                }
                 tokenizer::TokenKind::Comma => {}
-                tokenizer::TokenKind::QuoteString => {}
-                tokenizer::TokenKind::NonQuoteString => {}
+                tokenizer::TokenKind::QuoteString => {
+                    array.push(ASTNode::String(token.value.to_string()))
+                }
+                tokenizer::TokenKind::NonQuoteString => {
+                    return Err(format!("unexpected token: {:#?}", token).to_string());
+                }
                 tokenizer::TokenKind::NewLine => {}
-                tokenizer::TokenKind::Boolean => {}
-                tokenizer::TokenKind::Integer => {}
-                tokenizer::TokenKind::Float => {}
+                tokenizer::TokenKind::Boolean => {
+                    if token.value.to_lowercase() == "true".to_string() {
+                        array.push(ASTNode::Boolean(true));
+                    } else if token.value.to_lowercase() == "false".to_string() {
+                        array.push(ASTNode::Boolean(false));
+                    } else {
+                        return Err("something went terribly wrong! (most probably a language problem, not a code problem)".to_string());
+                    }
+                }
+                tokenizer::TokenKind::Integer => match token.value.parse::<i64>() {
+                    Ok(safe_value) => {
+                        array.push(ASTNode::Integer(safe_value));
+                    }
+                    Err(e) => {
+                        return Err(e.to_string());
+                    }
+                },
+                tokenizer::TokenKind::Float => match token.value.parse::<f64>() {
+                    Ok(safe_value) => {
+                        array.push(ASTNode::Float(safe_value));
+                    }
+                    Err(e) => {
+                        return Err(e.to_string());
+                    }
+                },
             }
         }
-        return array;
+        return Ok(array);
     }
 }
